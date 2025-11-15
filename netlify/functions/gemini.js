bd51ebdc4a98363b94c70db22e104e65c188fd51
ProdGenie <bhaskarchatterjee1@gmail.com>
2025-11-15 20:27:46 +0000

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -30,8 +30,8 @@ exports.handler = async function (event) {
     }
 
     const url =
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" +
-      apiKey;
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +
+  apiKey;
 
     const payload = {
       contents: [
